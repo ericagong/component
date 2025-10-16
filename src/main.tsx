555ce7f5fca4c7c6e5f62958b590f7a8b286e519
Erica <48196721+ericagong@users.noreply.github.com>
2025-10-16 11:42:41 +0900

    chore: 프로젝트 초기 개발 환경 설정(Node + pnpm + typscript + tsup + vite(dev))

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Accordion } from '@/components/Accordion';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Accordion />
+  </React.StrictMode>,
+);
