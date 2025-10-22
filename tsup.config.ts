a6bf590815aa68ab30d11b6ce68cf9be6156960e
Erica <48196721+ericagong@users.noreply.github.com>
2025-10-22 16:03:41 +0900

    chore(tsup): tsup 설정 파일 root 경로로 이동

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,15 +4,17 @@ import path from 'node:path';
 
 import { defineConfig } from 'tsup';
 
-const root = path.resolve(__dirname, '../..');
+console.log('🧩 [TSUP CONFIG] loaded ✅');
+console.log('cwd:', process.cwd());
+console.log('entry check:', path.resolve('src/index.ts'));
 
 export default defineConfig({
-  entry: [path.join(root, 'src/index.ts')],
+  entry: ['src/index.ts'], // 루트 기준
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
   clean: true,
-  outDir: path.join(root, 'dist'),
+  outDir: 'dist', // dist 역시 루트 기준
   target: 'esnext',
   minify: false,
   splitting: false,
