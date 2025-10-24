295cab77d9354fc304ed6b9d5fc35601251587f0
Erica <48196721+ericagong@users.noreply.github.com>
2025-10-24 09:58:22 +0900

    chore(.github/workflows): CI/CD 파이프라인 구성

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,12 +2,38 @@ export default {
   '*.{ts,tsx,js,jsx}': (files) => {
     const fileList = files.join(' ');
 
-    return [`pnpm lint:js:fix ${fileList}`, `pnpm format ${fileList}`];
+    return [
+      'echo ✨ Formatting source files with Prettier...',
+      // format
+      `pnpm format ${fileList}`,
+      'echo ✅ Formatting complete!',
+      // eslint
+      'echo 🧹 Linting and fixing source files with ESLint...',
+      `pnpm lint:js:fix ${fileList}`,
+      'echo ✅ Linting complete!',
+    ];
   },
+
   '*.{scss,css}': (files) => {
     const fileList = files.join(' ');
 
-    return [`pnpm lint:style:fix ${fileList}`, `pnpm format ${fileList}`];
+    return [
+      'echo ✨ Formatting style files with Prettier...',
+      `pnpm format ${fileList}`,
+      'echo ✅ Formatting complete!',
+      'echo 💅 Linting and fixing style files with Stylelint...',
+      `pnpm lint:style:fix ${fileList}`,
+      'echo ✅ Linting complete!',
+    ];
   },
-  '*.{md,json,yml,yaml}': ['pnpm format'],
+
+  '*.{md,json,yml,yaml}': (files) => {
+    const fileList = files.join(' ');
+
+    return [
+      'echo ✨ Formatting markdown and config files with Prettier...',
+      `pnpm format ${fileList}`,
+      'echo ✅ Formatting complete!',
+    ];
+  },
 };
